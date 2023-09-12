fbc7c7fd5c6793625daf8ea6bdd2cb0bfb571eda
Emily Valdez <emily.milan.valdez@gmail.com>
2023-09-12 08:43:09 -0500

    emv controls lab

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result;
+  if (someNumber <= 400){
+    result = "This one is on me!";
+  }
+  else if (someNumber <= 2000){
+    result = "That will be twenty bucks.";
+  }
+  else if (someNumber <= 2500){
+  result = "I will gladly take your thirty bucks.";
+  }
+  else if (someNumber > 2500){
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result = (city == "NYC") ? "Ok, sounds good." : "No go.";
+return result;
 }
+ternaryCheckCity("NYC");
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(charm){
+  let charmMessage 
+  switch (charm){
+    case "generous":
+      charmMessage = "Thank you so much.";
+        break;
+  case "not as generous":
+    charmMessage = "Thank you.";
+      break;
+  default:
+    charmMessage = "Bye.";
+    break;
+  }
+  return charmMessage;
 }
